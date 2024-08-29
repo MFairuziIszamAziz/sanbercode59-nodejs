d1c0cde4f5e477a3e06649610b29006dea36dfa1
MFairuziIszamAziz <m.kholidatmi@gmail.com>
2024-08-29 21:16:03 +0700

    hapus files env.ts

--- source-code-multer/src/utils/env.ts
+++
@@ -1,6 +0,0 @@
-export const CLOUDINARY_API_KEY: string =
-  process.env.CLOUDINARY_API_KEY || "993381228595143";
-export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "lH9MGJZxAe_YjIE1MBYSRD8Qoy4";
-export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "dryvld9k0";
